fdbdd1a31b238b0abf3b14a6115e11fb95a08d03
Szymon Bartanowicz <szymon@bartanowicz.pl>
2022-04-19 20:53:25 +0200

    updated Dropbox class

--- app/Dropbox.php
+++ app/Dropbox.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dropbox extends Model
 {
-    const APP_KEY = 'h82u3wzjz8fkwnx';
-    const APP_SECRET = 'ga8am5xcdq4jn14';
+    const APP_KEY = '';
+    const APP_SECRET = '';
     const REDIRECT_URI = 'http://localhost:8000/setDbxCredentials';
 }
